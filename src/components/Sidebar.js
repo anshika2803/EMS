70540d6958a3792b1a1a597fc63492eb6ef9d34c
Suritam-Nandy <13suritsuritam@gmail.com>
2021-10-24 12:56:59 +0530

    💄 sidebar

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,20 +1,28 @@
 import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
+import { RiDashboardLine } from "react-icons/ri";
+import { AiOutlineLeft } from "react-icons/ai";
 
 const Sidebar = () => {
   const sidebarList = [
     {
-      name: "Home",
-      notificationCount: 0,
-      link: "",
+      name: "Dashboard",
+      icon: <RiDashboardLine />,
+      moreOptions: false,
+      link: "dashboard",
     },
-    { name: "Profile", notificationCount: 2, link: "dashboard" },
-    { name: "Listed Spaces", notificationCount: 0, link: "listedspaces" },
+    {
+      name: "Misc",
+      notificationCount: 0,
+      link: "dashboard",
+      moreOptions: true,
+    },
+    { name: "Students", notificationCount: 2, link: "dashboard" },
 
-    { name: "Interested Customers", notificationCount: 0, link: "" },
-    { name: "Add Space", notificationCount: 0, link: "addspace" },
-    { name: "Calendar", notificationCount: 0, link: "dashboard" },
-    { name: "Payments", notificationCount: 0, link: "payments" },
+    { name: "Shopping", notificationCount: 0, link: "dashboard" },
+    { name: "Store", notificationCount: 0, link: "dashboard" },
+    { name: "Institute", notificationCount: 0, link: "dashboard" },
+    { name: "Manage Test", notificationCount: 0, link: "dashboard" },
   ];
 
   const [open, setOpen] = useState(false);
@@ -26,35 +34,38 @@ const Sidebar = () => {
       <div
         className={`${
           open ? "sm:flex" : "hidden"
-        }  w-1/6 absolute sm:relative bg-gray-800 shadow md:h-full flex-col justify-between  sm:flex h-screen pb-72 bg-white`}
+        }  w-1/6 absolute sm:relative shadow md:h-full flex-col justify-between  sm:flex h-screen pb-72 bg-white`}
       >
         <div className="px-3 ">
-          <div className="w-full mt-1.5 flex items-center flex-col">
-            <div className=" w-12 h-12">
+          <div className="w-full mt-1.5 flex items-center flex-row">
+            <div className=" w-20 h-20">
               <img
                 className="rounded-full border border-gray-100 shadow-sm"
                 src="https://randomuser.me/api/portraits/women/81.jpg"
                 alt="user "
               />
             </div>
-            <div className=" h-3 w-3 my-1 border-2 border-white rounded-full bg-green-400 z-2"></div>
-
-            <span className="text-sm">uid</span>
+            <div className="ml-2">
+              <h1 className="text-lg font-medium ml-0.5 mb-0">Akash</h1>
+              <div className="flex flex-row">
+                <div className=" h-3 w-3 my-1 border-2 border-white rounded-full bg-green-400 "></div>
+                <span className="text-sm ml-0.5"> Online</span>
+              </div>
+            </div>
           </div>
-          <ul className="mt-12">
+          <ul className="mt-6 ">
+            <p className="text-gray-500 mb-4">MAIN NAVIGATION</p>
             {sidebarList.map((item, index) => {
               return (
-                <Link to={`/${item.link}`}>
-                  <li className="flex w-full justify-between text-gray-300 hover:text-gray-500 cursor-pointer items-center mb-6">
-                    <div className="flex items-center">
+                <Link to={`/${item.link}`} className="hover:no-underline">
+                  <li className="flex w-full justify-between text-gray-700 hover:text-gray-900 cursor-pointer items-center mb-6">
+                    <div className="flex items-center font-semibold">
                       {item.icon}
-                      <span className="text-sm text-red-800 ml-2">
-                        {item.name}
-                      </span>
+                      <span className="text-sm ml-2 ">{item.name}</span>
                     </div>
-                    {item.notificationCount > 0 && (
-                      <div className="py-1 px-3 bg-gray-700 rounded text-gray-500 flex items-center justify-center text-xs">
-                        {item.notificationCount}
+                    {item.moreOptions > 0 && (
+                      <div className="py-1 px-3 rounded text-gray-500 flex items-center justify-center">
+                        <AiOutlineLeft />
                       </div>
                     )}
                   </li>
--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -18,28 +18,28 @@ const Dashboard = () => {
                 <h1 className=" text-4xl m-2">2</h1>
                 <p className="text-xl m-2">Total Tests</p>
                 <div className="bottom-0 flex justify-center items-center bg-cyan-500 mt-4">
-                  <p>more info</p>
+                  <span>more info</span>
                 </div>
               </div>
               <div className=" h-32 w-80 m-2 bg-green-600 text-white">
                 <h1 className=" text-4xl m-2">18</h1>
                 <p className="text-xl m-2">Total Test Package</p>
                 <div className="bottom-0 flex justify-center items-center bg-green-700 mt-4">
-                  <p>more info</p>
+                  <span>more info</span>
                 </div>
               </div>
               <div className=" h-32 w-80 m-2 bg-orange-400 text-white">
                 <h1 className=" text-4xl m-2">4</h1>
                 <p className="text-xl m-2">User Registrations</p>
                 <div className="bottom-0 flex justify-center items-center bg-orange-500 mt-4">
-                  <p>more info</p>
+                  <span>more info</span>
                 </div>
               </div>
               <div className=" h-32 w-80 m-2 bg-red-500 text-white">
                 <h1 className=" text-4xl m-2">7</h1>
                 <p className="text-xl m-2">Total Questions</p>
                 <div className="bottom-0 flex justify-center items-center bg-red-600 mt-4">
-                  <p>more info</p>
+                  <span>more info</span>
                 </div>
               </div>
             </div>
